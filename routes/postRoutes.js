b6beaa9f4390ac7ec417724c77a90cfeaecb1901
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-06-22 23:22:49 +0100

    postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,1 +1,15 @@
+const router =
+require("express").Router();
 
+const {
+createPost,
+getPosts
+}
+=
+require("../controllers/postController");
+
+router.post("/create",createPost);
+
+router.get("/",getPosts);
+
+module.exports = router;
