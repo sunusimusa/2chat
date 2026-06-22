3f719da3002ef081f15a637aed7234095f98bbca
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-06-22 08:06:52 +0100

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+const app = express();
+
+app.use(express.json());
+
+// Home Route
+app.get("/", (req, res) => {
+  res.send("🚀 2Chat Server Running Successfully");
+});
+
+// MongoDB Connect
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB Error:", err.message);
+  });
