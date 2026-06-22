3a11406a5daf9c4c2c182e3ca3210671023d6fb8
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-06-22 23:57:42 +0100

    postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -59,3 +59,54 @@ message:err.message
 }
 
 };
+
+// LIKE POST
+
+exports.likePost = async (req,res)=>{
+
+try{
+
+const { postId, username } = req.body;
+
+const post =
+await Post.findById(postId);
+
+if(!post){
+
+return res.status(404).json({
+success:false,
+message:"Post not found"
+});
+
+}
+
+if(post.likes.includes(username)){
+
+post.likes =
+post.likes.filter(
+u => u !== username
+);
+
+}else{
+
+post.likes.push(username);
+
+}
+
+await post.save();
+
+res.json({
+success:true,
+likes:post.likes.length
+});
+
+}catch(err){
+
+res.status(500).json({
+success:false,
+message:err.message
+});
+
+}
+
+};
