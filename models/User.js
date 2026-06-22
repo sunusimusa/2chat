8944b3505d4be11c589c2a94e284abd163bca4ea
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-06-22 12:53:03 +0100

    User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,12 @@
-{
- username,
- fullName,
- email,
- password,
- avatar,
- bio,
- followers,
- following,
- createdAt
-}
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  avatar: String
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model("User", userSchema);
