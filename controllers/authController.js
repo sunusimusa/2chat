b943cdfc61eb2173104276a462ac1330e667cee0
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-06-22 13:06:12 +0100

    authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,83 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const generateToken = require("../utils/generateToken");
 
+// REGISTER
+exports.register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const userExists = await User.findOne({
+      $or: [{ email }, { username }]
+    });
+
+    if (userExists) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists"
+      });
+    }
+
+    const hashedPassword =
+      await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({
+      success: true,
+      token: generateToken(user._id),
+      user
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+};
+
+// LOGIN
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials"
+      });
+    }
+
+    const match =
+      await bcrypt.compare(
+        password,
+        user.password
+      );
+
+    if (!match) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials"
+      });
+    }
+
+    res.json({
+      success: true,
+      token: generateToken(user._id),
+      user
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+};
