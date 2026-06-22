b14bdbe16fd5e1669808407f6b702ffbffbe87d4
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-06-22 15:26:18 +0100

    User.js
    Updated user schema to include required fields and defaults.

--- models/User.js
+++ models/User.js
@@ -1,12 +1,37 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
-  avatar: String
-}, {
-  timestamps: true
-});
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    },
+
+    avatar: {
+      type: String,
+      default: ""
+    },
+
+    bio: {
+      type: String,
+      default: ""
+    }
+  },
+  {
+    timestamps: true
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
