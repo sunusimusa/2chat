739e9bc7c1b6967885ec2502db217c271e336df2
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-06-22 15:09:55 +0100

    authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,15 @@ const {
   login
 } = require("../controllers/authController");
 
+// TEST
+router.get("/", (req, res) => {
+  res.send("Auth API Working");
+});
+
+// REGISTER
 router.post("/register", register);
+
+// LOGIN
 router.post("/login", login);
 
 module.exports = router;
