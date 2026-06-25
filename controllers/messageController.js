4c2f3f846bb8884737809b69b4e776007ef33a41
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-06-25 09:36:31 +0100

    messageController.js

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,1 +1,83 @@
+const Message =
+require("../models/Message");
 
+// SEND MESSAGE
+
+exports.sendMessage =
+async(req,res)=>{
+
+try{
+
+const {
+sender,
+receiver,
+text
+} = req.body;
+
+const message =
+await Message.create({
+sender,
+receiver,
+text
+});
+
+res.json({
+success:true,
+message
+});
+
+}catch(err){
+
+res.status(500).json({
+success:false,
+message:err.message
+});
+
+}
+
+};
+
+// GET CHAT
+
+exports.getMessages =
+async(req,res)=>{
+
+try{
+
+const {
+sender,
+receiver
+} = req.query;
+
+const messages =
+await Message.find({
+$or:[
+{
+sender,
+receiver
+},
+{
+sender:receiver,
+receiver:sender
+}
+]
+})
+.sort({
+createdAt:1
+});
+
+res.json({
+success:true,
+messages
+});
+
+}catch(err){
+
+res.status(500).json({
+success:false,
+message:err.message
+});
+
+}
+
+};
