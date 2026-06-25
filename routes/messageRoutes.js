dfa3b34939d231d5420ccd33fbc1ac1f79de1c75
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-06-25 09:43:09 +0100

    messageRoutes.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,1 +1,23 @@
+const router =
+require("express").Router();
 
+const {
+sendMessage,
+getMessages
+}
+=
+require("../controllers/messageController");
+
+// Send Message
+router.post(
+"/send",
+sendMessage
+);
+
+// Get Chat
+router.get(
+"/chat",
+getMessages
+);
+
+module.exports = router;
