e83a2c3af8486b392d6cecfa26cc9ae6090d90e0
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-06-27 13:01:34 +0100

    server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
