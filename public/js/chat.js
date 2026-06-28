cfc832ff235d59aa0a8cc319ecd09223fe67b451
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-06-28 18:56:57 +0100

    chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,138 @@
+<script>
+
+const user =
+JSON.parse(localStorage.getItem("user"));
+
+const socket = io();
+
+socket.emit("join", user.username);
+
+const params =
+new URLSearchParams(window.location.search);
+
+const receiver =
+params.get("user");
+
+document.getElementById("chatUser").innerText =
+receiver;
+
+async function loadMessages(){
+
+if(!receiver) return;
+
+const res =
+await fetch(
+`/api/messages/chat?sender=${user.username}&receiver=${receiver}`
+);
+
+const data =
+await res.json();
+
+let html="";
+
+data.messages.forEach(msg=>{
+
+const mine =
+msg.sender===user.username;
+html+=`
+
+<div class="${mine?"me":"other"}">
+
+<div class="${mine?"bubble-me":"bubble-other"}">
+
+${msg.text}
+
+${
+mine
+?
+`<br>
+<small style="font-size:11px;opacity:.8;">
+${msg.seen ? "👁 Seen" : "✓ Sent"}
+</small>`
+:
+""
+}
+
+</div>
+
+</div>
+
+`;
+
+});
+
+const chat =
+document.getElementById("chat");
+
+chat.innerHTML=html;
+
+chat.scrollTop=
+chat.scrollHeight;
+
+}
+
+async function sendMessage(){
+
+const text =
+document.getElementById("message").value.trim();
+
+if(text==="") return;
+
+const res =
+await fetch(
+"/api/messages/send",
+{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+sender:user.username,
+receiver:receiver,
+text:text
+})
+}
+);
+
+const data =
+await res.json();
+
+if(data.success){
+
+socket.emit("newMessage",{
+sender:user.username,
+receiver,
+text
+});
+
+document.getElementById("message").value="";
+
+loadMessages();
+
+}else{
+
+alert(data.message);
+
+}
+
+}
+
+loadMessages();
+
+setInterval(loadMessages,2000);
+
+socket.on("receiveMessage",(msg)=>{
+
+if(
+msg.sender===receiver ||
+msg.receiver===receiver
+){
+
+loadMessages();
+
+}
+
+});
+
+</script>
 
