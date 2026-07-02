ffe2d1a8b28bdd3c2f5483ff5d489acfa114cac7
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-02 16:26:16 +0100

    userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,12 @@
+const router = require("express").Router();
 
+const {
+followUser
+} = require("../controllers/userController");
+
+router.put(
+"/follow",
+followUser
+);
+
+module.exports = router;
