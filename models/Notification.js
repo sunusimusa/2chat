946fb07f7ad0c55c86844cc56c901fd06a5e1bc8
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-04 06:50:46 +0100

    Notification.js

--- models/Notification.js
+++ models/Notification.js
@@ -1,5 +1,50 @@
-{
- userId,
- title,
- read
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema({
+
+receiver:{
+type:String,
+required:true
+},
+
+sender:{
+type:String,
+required:true
+},
+
+type:{
+type:String,
+enum:[
+"like",
+"comment",
+"follow",
+"message"
+],
+required:true
+},
+
+postId:{
+type:mongoose.Schema.Types.ObjectId,
+ref:"Post",
+default:null
+},
+
+text:{
+type:String,
+default:""
+},
+
+read:{
+type:Boolean,
+default:false
 }
+
+},{
+timestamps:true
+});
+
+module.exports =
+mongoose.model(
+"Notification",
+notificationSchema
+);
