b7d0490487ad94804b0121e5c44277c681e94f96
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-08 15:21:29 +0100

    FriendRequest.js

--- models/FriendRequest.js
+++ models/FriendRequest.js
@@ -1,1 +1,30 @@
+const mongoose = require("mongoose");
 
+const friendRequestSchema = new mongoose.Schema(
+{
+    sender: {
+        type: String,
+        required: true
+    },
+
+    receiver: {
+        type: String,
+        required: true
+    },
+
+    status: {
+        type: String,
+        enum: ["pending", "accepted", "rejected"],
+        default: "pending"
+    }
+
+},
+{
+    timestamps: true
+}
+);
+
+module.exports = mongoose.model(
+    "FriendRequest",
+    friendRequestSchema
+);
