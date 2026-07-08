c677c8e25072e641a4e6df5fdf8b1c255eada88b
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-08 15:27:54 +0100

    friendRoutes.js

--- routes/friendRoutes.js
+++ routes/friendRoutes.js
@@ -1,1 +1,43 @@
+const router = require("express").Router();
 
+const {
+
+sendRequest,
+acceptRequest,
+rejectRequest,
+getRequests,
+getFriends
+
+} = require("../controllers/friendController");
+
+// Send Friend Request
+router.post(
+"/send",
+sendRequest
+);
+
+// Accept Friend Request
+router.post(
+"/accept",
+acceptRequest
+);
+
+// Reject Friend Request
+router.post(
+"/reject",
+rejectRequest
+);
+
+// Get Pending Requests
+router.get(
+"/requests/:username",
+getRequests
+);
+
+// Get Friends List
+router.get(
+"/list/:username",
+getFriends
+);
+
+module.exports = router;
