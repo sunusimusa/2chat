e65a13b5c22501b7fede9e59dff7aa06e95c341d
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-09 18:58:01 +0100

    Status.js

--- models/Status.js
+++ models/Status.js
@@ -1,1 +1,36 @@
+const mongoose = require("mongoose");
 
+const statusSchema = new mongoose.Schema({
+
+username:{
+type:String,
+required:true
+},
+
+type:{
+type:String,
+enum:["image","video","text"],
+default:"image"
+},
+
+media:{
+type:String,
+default:""
+},
+
+text:{
+type:String,
+default:""
+},
+
+views:{
+type:[String],
+default:[]
+}
+
+},{
+timestamps:true
+});
+
+module.exports =
+mongoose.model("Status",statusSchema);
