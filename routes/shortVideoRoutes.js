2a9aa66fd771e07e99d2a3bc8299a8794d2361e6
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-10 15:42:03 +0100

    shortVideoRoutes.js

--- routes/shortVideoRoutes.js
+++ routes/shortVideoRoutes.js
@@ -1,1 +1,29 @@
+const router = require("express").Router();
 
+const {
+
+    uploadVideo,
+    getVideos,
+    likeVideo,
+    commentVideo,
+    addView
+
+} = require("../controllers/shortVideoController");
+
+
+// Upload Video
+router.post("/upload", uploadVideo);
+
+// Get All Videos
+router.get("/all", getVideos);
+
+// Like Video
+router.put("/like", likeVideo);
+
+// Comment
+router.put("/comment", commentVideo);
+
+// Add View
+router.put("/view/:id", addView);
+
+module.exports = router;
