be31ef7a704fd0f004a1df243ed26ff39543b1b8
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-10 15:35:51 +0100

    ShortVideo.js

--- models/ShortVideo.js
+++ models/ShortVideo.js
@@ -1,1 +1,48 @@
+const mongoose = require("mongoose");
 
+const shortVideoSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        required: true
+    },
+
+    avatar: {
+        type: String,
+        default: ""
+    },
+
+    caption: {
+        type: String,
+        default: ""
+    },
+
+    video: {
+        type: String,
+        required: true
+    },
+
+    likes: {
+        type: [String],
+        default: []
+    },
+
+    comments: [{
+        username: String,
+        text: String,
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }],
+
+    views: {
+        type: Number,
+        default: 0
+    }
+
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("ShortVideo", shortVideoSchema);
