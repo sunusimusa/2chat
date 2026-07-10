7fa1a25383ecf7090578a06682545ad5a3c8a6ac
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-10 11:11:01 +0100

    statusRoutes.js

--- routes/statusRoutes.js
+++ routes/statusRoutes.js
@@ -1,19 +1,18 @@
 const router = require("express").Router();
 
 const {
-
-createStatus,
-
-getStatuses,
-
-getStatusById
-
+  createStatus,
+  getStatuses,
+  getStatusById
 } = require("../controllers/statusController");
 
+// CREATE
 router.post("/create", createStatus);
 
+// ALL STATUS
 router.get("/all", getStatuses);
 
+// SINGLE STATUS
 router.get("/:id", getStatusById);
 
 module.exports = router;
