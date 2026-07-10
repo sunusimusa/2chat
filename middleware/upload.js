6b670c985a012907cf4997b35a9d3317bb838bc1
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-10 18:02:23 +0100

    upload.js
    Refactor multer configuration to include file size limit.

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,8 +1,17 @@
-const multer =
-require("multer");
+const multer = require("multer");
 
-const storage =
-multer.diskStorage({});
+const storage = multer.diskStorage({});
 
-module.exports =
-multer({ storage });
+const upload = multer({
+
+    storage,
+
+    limits: {
+
+        fileSize: 20 * 1024 * 1024 // 20MB
+
+    }
+
+});
+
+module.exports = upload;
