81d353464ccc6f49866646e41e1867d8f72ef1e5
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-16 07:30:44 +0100

    upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,17 +3,36 @@ const multer = require("multer");
 const storage = multer.memoryStorage();
 
 const upload = multer({
-  storage,
-  limits: {
-    fileSize: 20 * 1024 * 1024
-  },
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype.startsWith("video/")) {
-      cb(null, true);
-    } else {
-      cb(new Error("Only video files are allowed"));
-    }
-  }
+
+storage,
+
+limits: {
+
+fileSize: 30 * 1024 * 1024
+
+},
+
+fileFilter: (req,file,cb)=>{
+
+
+if(
+file.mimetype.startsWith("video/") ||
+file.mimetype.startsWith("image/") ||
+file.mimetype.startsWith("audio/")
+){
+
+cb(null,true);
+
+}else{
+
+cb(new Error("Unsupported file type"));
+
+}
+
+
+}
+
 });
+
 
 module.exports = upload;
