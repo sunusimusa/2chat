732c0167f85a5929378cda1fdc67ca17da8f4c91
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-20 13:23:02 +0100

    webrtc.js

---
+++ public/js/webrtc.js
@@ -0,0 +1,43 @@
+// =======================
+// 2Chat WebRTC Engine
+// =======================
+
+let localStream = null;
+let remoteStream = null;
+let peerConnection = null;
+
+const rtcConfig = {
+
+iceServers: [
+
+{
+urls: "stun:stun.l.google.com:19302"
+}
+
+]
+
+};
+
+async function createPeer(){
+
+peerConnection = new RTCPeerConnection(rtcConfig);
+
+remoteStream = new MediaStream();
+
+document.getElementById("remoteAudio").srcObject =
+remoteStream;
+
+// Remote Audio
+peerConnection.ontrack = (event)=>{
+
+event.streams[0].getTracks().forEach(track=>{
+
+remoteStream.addTrack(track);
+
+});
+
+};
+
+console.log("✅ PeerConnection Ready");
+
+}
