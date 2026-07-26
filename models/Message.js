488d60e8a8744dbf62ba1f97885bacd5ff6a915f
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-26 16:31:05 +0100

    Message.js

--- models/Message.js
+++ models/Message.js
@@ -2,98 +2,122 @@ const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema(
 {
-  sender:{
-    type:String,
-    required:true
-  },
+    sender:{
+        type:String,
+        required:true
+    },
 
-  receiver:{
-    type:String,
-    required:true
-  },
+    receiver:{
+        type:String,
+        required:true
+    },
 
-  text:{
-type:String,
-default:""
+    text:{
+        type:String,
+        default:""
+    },
+
+    image:{
+        type:String,
+        default:""
+    },
+
+    voice:{
+        type:String,
+        default:""
+    },
+
+    voiceDuration:{
+        type:Number,
+        default:0
+    },
+
+    replyTo:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Message",
+        default:null
+    },
+
+    replyText:{
+        type:String,
+        default:""
+    },
+
+    replyUser:{
+        type:String,
+        default:""
+    },
+
+    replyImage:{
+        type:String,
+        default:""
+    },
+
+    replyVoice:{
+        type:String,
+        default:""
+    },
+
+    reactions:{
+        type:[
+            {
+                username:{
+                    type:String,
+                    default:""
+                },
+                emoji:{
+                    type:String,
+                    default:""
+                }
+            }
+        ],
+        default:[]
+    },
+
+    seen:{
+        type:Boolean,
+        default:false
+    },
+
+    seenAt:{
+        type:Date,
+        default:null
+    },
+
+    delivered:{
+        type:Boolean,
+        default:false
+    },
+
+    deliveredAt:{
+        type:Date,
+        default:null
+    },
+
+    deletedForEveryone:{
+        type:Boolean,
+        default:false
+    },
+
+    deletedBy:{
+        type:String,
+        default:""
+    }
+
 },
-
-image:{
-type:String,
-default:""
-},
-
-  voice:{
-type:String,
-default:""
-},
-
-  voiceDuration:{
-type:Number,
-default:0
-},
-
-  replyTo:{
-type:mongoose.Schema.Types.ObjectId,
-ref:"Message",
-default:null
-},
-
-replyText:{
-type:String,
-default:""
-},
-
-replyUser:{
-type:String,
-default:""
-},
-
-  reactions:{
-type:[
 {
-username:String,
-emoji:String
-}
-],
-default:[]
-},
-
-  seen:{
-    type:Boolean,
-    default:false
-  },
-
-  delivered:{
-type:Boolean,
-default:false
-},
-
-deliveredAt:{
-type:Date
-},
-
-  deletedForEveryone: {
-    type: Boolean,
-    default: false
-},
-
-  deletedBy: {
-    type: String,
-    default: ""
-},
-
-  seenAt:{
-    type:Date
-  }
-
-},
-{
-  timestamps:true
+    timestamps:true
 }
 );
 
-module.exports =
-mongoose.model(
-"Message",
-messageSchema
+// Chat loading ya zama da sauri
+messageSchema.index({
+    sender:1,
+    receiver:1,
+    createdAt:1
+});
+
+module.exports = mongoose.model(
+    "Message",
+    messageSchema
 );
