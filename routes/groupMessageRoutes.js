c642acf8d66eb5196396248b2db8dc5d24d629cb
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-29 21:38:23 +0100

    groupMessageRoutes.js

--- routes/groupMessageRoutes.js
+++ routes/groupMessageRoutes.js
@@ -1,1 +1,21 @@
+const router = require("express").Router();
 
+const {
+
+    sendMessage,
+
+    getMessages
+
+} = require("../controllers/groupMessageController");
+
+/* ==========================
+GROUP MESSAGES
+========================== */
+
+// Send Group Message
+router.post("/send", sendMessage);
+
+// Load Group Messages
+router.get("/:groupId", getMessages);
+
+module.exports = router;
