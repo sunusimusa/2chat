c7567b5472677c0bed52fe73d3ed2bf0ea78810c
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-29 11:52:58 +0100

    home.js

--- public/js/home.js
+++ public/js/home.js
@@ -663,3 +663,21 @@ alert("✅ Link copied");
 }
 
 }
+
+function openMenu(){
+
+document.getElementById("sideMenu").classList.add("active");
+
+document.getElementById("menuOverlay").classList.add("active");
+
+}
+
+function closeMenu(){
+
+document.getElementById("sideMenu").classList.remove("active");
+
+document.getElementById("menuOverlay").classList.remove("active");
+
+}
+
+
