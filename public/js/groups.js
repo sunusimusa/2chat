b3618c9cef99c24928418cc72067b1e3632c6e5e
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-29 18:06:09 +0100

    groups.js
    Added HTML structure for displaying group cards with names and member counts.

---
+++ public/js/groups.js
@@ -0,0 +1,50 @@
+const groupList =
+document.getElementById("groupList");
+
+groupList.innerHTML = `
+
+<div class="group-card">
+
+<img src="/images/default-group.png">
+
+<div>
+
+<div class="group-name">
+
+2Chat Community
+
+</div>
+
+<div class="group-members">
+
+1,240 Members
+
+</div>
+
+</div>
+
+</div>
+
+<div class="group-card">
+
+<img src="/images/default-group.png">
+
+<div>
+
+<div class="group-name">
+
+Developers
+
+</div>
+
+<div class="group-members">
+
+230 Members
+
+</div>
+
+</div>
+
+</div>
+
+`;
