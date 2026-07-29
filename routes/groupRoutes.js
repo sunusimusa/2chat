6ba46672157cc6539e9633207adcdc6673ceff22
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-29 09:49:31 +0100

    groupRoutes.js

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -1,1 +1,26 @@
+const router = require("express").Router();
 
+const {
+    createGroup,
+    getGroups,
+    getGroup,
+    joinGroup,
+    leaveGroup
+} = require("../controllers/groupController");
+
+// Create Group
+router.post("/create", createGroup);
+
+// Get all groups
+router.get("/all", getGroups);
+
+// Get single group
+router.get("/:id", getGroup);
+
+// Join group
+router.put("/join", joinGroup);
+
+// Leave group
+router.put("/leave", leaveGroup);
+
+module.exports = router;
