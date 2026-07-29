41a44b991ad761a6259f7d4c8edb3e67aba7d827
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-07-29 21:33:08 +0100

    GroupMessage.js

--- models/GroupMessage.js
+++ models/GroupMessage.js
@@ -1,1 +1,82 @@
+const mongoose = require("mongoose");
 
+const groupMessageSchema = new mongoose.Schema({
+
+    groupId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Group",
+        required:true
+    },
+
+    sender:{
+        type:String,
+        required:true
+    },
+
+    text:{
+        type:String,
+        default:""
+    },
+
+    image:{
+        type:String,
+        default:""
+    },
+
+    voice:{
+        type:String,
+        default:""
+    },
+
+    voiceDuration:{
+        type:Number,
+        default:0
+    },
+
+    replyTo:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"GroupMessage",
+        default:null
+    },
+
+    replyUser:{
+        type:String,
+        default:""
+    },
+
+    replyText:{
+        type:String,
+        default:""
+    },
+
+    replyImage:{
+        type:String,
+        default:""
+    },
+
+    reactions:[
+
+        {
+
+            username:String,
+
+            emoji:String
+
+        }
+
+    ],
+
+    deleted:{
+        type:Boolean,
+        default:false
+    }
+
+},{
+    timestamps:true
+});
+
+module.exports =
+mongoose.model(
+    "GroupMessage",
+    groupMessageSchema
+);
