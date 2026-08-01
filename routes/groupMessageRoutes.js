28d23109069117bc43676781b6287c410a3736a7
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-08-01 07:33:50 +0100

    groupMessageRoutes.js

--- routes/groupMessageRoutes.js
+++ routes/groupMessageRoutes.js
@@ -3,24 +3,39 @@ const multer = require("multer");
 
 const storage = multer.memoryStorage();
 
-const upload = multer({ storage });
+const upload = multer({
+    storage
+});
 
 const {
-
     sendMessage,
-
     getMessages
-
 } = require("../controllers/groupMessageController");
 
 /* ==========================
 GROUP MESSAGES
 ========================== */
 
-// Send Group Message
-router.post("/send", upload.single("file"), sendMessage);
+// Send Text + Image + Voice
+router.post(
+    "/send",
+    upload.fields([
+        {
+            name: "image",
+            maxCount: 1
+        },
+        {
+            name: "voice",
+            maxCount: 1
+        }
+    ]),
+    sendMessage
+);
 
 // Load Group Messages
-router.get("/:groupId", getMessages);
+router.get(
+    "/:groupId",
+    getMessages
+);
 
 module.exports = router;
