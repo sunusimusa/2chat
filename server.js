488033acffa647eacfd55492afa6e4856560e580
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-08-10 10:21:18 +0100

    server.js
    Increase JSON and URL-encoded body size limits for requests.

--- server.js
+++ server.js
@@ -35,7 +35,15 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname,"public")));
 app.use(cors());
-app.use(express.json());
+
+app.use(express.json({
+    limit: "15mb"
+}));
+
+app.use(express.urlencoded({
+    extended: true,
+    limit: "15mb"
+}));
 
 app.use("/api/auth",authRoutes);
 app.use("/api/posts",postRoutes);
