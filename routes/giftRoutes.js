19c7b25f54c62682139d7feac33bee951e9e065b
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-08-12 14:04:57 +0100

    giftRoutes.js
    Added a route to get received gifts.

--- routes/giftRoutes.js
+++ routes/giftRoutes.js
@@ -3,7 +3,8 @@ const router = require("express").Router();
 const protect = require("../middleware/auth");
 
 const {
-  sendGift
+  sendGift,
+  getReceivedGifts
 } = require("../controllers/giftController");
 
 
@@ -15,6 +16,17 @@ router.post(
   "/send",
   protect,
   sendGift
+);
+
+
+// =========================================
+// RECEIVED GIFTS
+// =========================================
+
+router.get(
+  "/received",
+  protect,
+  getReceivedGifts
 );
 
 
