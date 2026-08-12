9299e702887aef7733b3ab1c696488dd286ce851
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-08-12 07:15:56 +0100

    giftRoutes.js

--- routes/giftRoutes.js
+++ routes/giftRoutes.js
@@ -1,1 +1,21 @@
+const router = require("express").Router();
 
+const protect = require("../middleware/auth");
+
+const {
+  sendGift
+} = require("../controllers/giftController");
+
+
+// =========================================
+// SEND GIFT
+// =========================================
+
+router.post(
+  "/send",
+  protect,
+  sendGift
+);
+
+
+module.exports = router;
