d7d9f3e260653962689c0fd0fe7d69778300004e
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-08-16 15:16:45 +0100

    withdrawalRoutes.js

--- routes/withdrawalRoutes.js
+++ routes/withdrawalRoutes.js
@@ -1,1 +1,21 @@
+const router = require("express").Router();
 
+const protect = require("../middleware/auth");
+
+const {
+    createWithdrawal
+} = require("../controllers/withdrawalController");
+
+
+// =========================================
+// CREATE WITHDRAWAL REQUEST
+// =========================================
+
+router.post(
+    "/",
+    protect,
+    createWithdrawal
+);
+
+
+module.exports = router;
