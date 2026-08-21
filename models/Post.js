d9c0ecc29d65bdcf154d532499357b46ac3996fd
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-08-21 09:23:28 +0100

    Post.js

--- models/Post.js
+++ models/Post.js
@@ -2,51 +2,56 @@ const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
 
-  userId:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User"
-  },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
 
-  username:{
-    type:String
-  },
+    username: {
+        type: String
+    },
 
-  avatar:{
-    type:String,
-    default:""
-  },
+    avatar: {
+        type: String,
+        default: ""
+    },
 
-  text:{
-    type:String
-  },
+    text: {
+        type: String,
+        default: ""
+    },
 
-  image:{
-type:String,
-default:""
-},
+    image: {
+        type: String,
+        default: ""
+    },
 
-  likes:{
-    type:[String],
-    default:[]
-  },
+    video: {
+        type: String,
+        default: ""
+    },
 
-  comments:{
-    type:[
-      {
-        username:String,
-        text:String,
-        createdAt:{
-          type:Date,
-          default:Date.now
-        }
-      }
-    ],
-    default:[]
-  }
+    likes: {
+        type: [String],
+        default: []
+    },
 
-},
-{
-  timestamps:true
+    comments: {
+        type: [
+            {
+                username: String,
+                text: String,
+                createdAt: {
+                    type: Date,
+                    default: Date.now
+                }
+            }
+        ],
+        default: []
+    }
+
+}, {
+    timestamps: true
 });
 
 module.exports =
