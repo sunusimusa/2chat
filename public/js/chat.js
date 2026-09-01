b5654ff7d54bb68fdd8f770c05f7c0bcd35cd2fc
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-09-01 14:39:22 +0100

    chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1096,14 +1096,10 @@ async function startRecording() {
     try {
 
         mediaStream =
-    await navigator.mediaDevices.getUserMedia({
-        audio: {
-            echoCancellation: true,
-            noiseSuppression: true,
-            autoGainControl: true
-        }
-    });
-
+            await navigator.mediaDevices.getUserMedia({
+                audio: true
+            });
+       
         let mimeType = "";
 
         if (
